0b161a0b2001d73ac31d969d9b01cca46490ca0f
you name <you@example.com>
2017-11-05 19:48:53 +0800

    添加ES6转义包

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import FastClick from 'fastclick'
 import './lib/flexbile'
 
 Vue.config.productionTip = false
+FastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
